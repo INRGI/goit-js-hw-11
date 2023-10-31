d5aab4415032b49460fcfc925042166cd93588ce
Ruslan <120032162+INRGI@users.noreply.github.com>
2023-10-31 15:33:29 +0100

    add markup to index.html

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import Notiflix from 'notiflix';
+
+const searchSelect = document.querySelector('.search-form');
+const gallerySelect = document.querySelector('.gallery');
