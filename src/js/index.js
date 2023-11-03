834f2ff8dc4796118c57334e341cd4fb53d05158
Ruslan <120032162+INRGI@users.noreply.github.com>
2023-11-03 10:58:14 +0100

    onSubmit and render

--- src/js/index.js
+++ src/js/index.js
@@ -29,7 +29,8 @@ function onSubmit(event) {
             if (data.totalHits === 0) {
                 Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
             } else {
-                // RENDER
+                renderGallery(data.hits);
+                simpleLightBox = new SimpleLightbox('.gallery a').refresh();
                 Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
             }
         })
@@ -39,3 +40,23 @@ function onSubmit(event) {
         })
 }
 function onLoadMore() { };
+
+function renderGallery(images) {
+    const markup = images.map(image => {
+        const { id, largeImageURL, webformatURL, tags, likes, views, comments, downloads } = image;
+        return `
+        <a class="gallery__link" href="${largeImageURL}">
+          <div class="gallery-item" id="${id}">
+            <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <div class="info">
+              <p class="info-item"><b>Likes</b>${likes}</p>
+              <p class="info-item"><b>Views</b>${views}</p>
+              <p class="info-item"><b>Comments</b>${comments}</p>
+              <p class="info-item"><b>Downloads</b>${downloads}</p>
+            </div>
+          </div>
+        </a>`
+    }).join('');
+
+    gallerySelect.insertAdjacentHTML('beforeend', markup);
+}
--- src/js/pixabay-service.js
+++ src/js/pixabay-service.js
@@ -1,15 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = 'https://pixabay.com/api/';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 const API_KEY = '40331003-99a3d5cb80482624416a8cb30';
 
-const searchParams = new searchParams({
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
-    per_page: 40
-});
-
 export async function getImages(query, page) {
-    return await axios.get(`${BASE_URL}?key=${API_KEY}&q=${query}&${searchParams}&page=${page}`)
+  const response = await axios.get(
+    `?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`);
+  return response;
 }
